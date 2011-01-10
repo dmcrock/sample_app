37d5ddb3e54cf1420fb085489613d266ff91ab6a
unknown <dclelland@.rivermarkcu.local>
2011-01-10 15:06:16 -0800

    fixed relationship controller, bad syntax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,6 @@
 class RelationshipsController < ApplicationController
   before_filter :authenticate
-  
+
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
@@ -9,12 +9,13 @@ class RelationshipsController < ApplicationController
       format.js
     end
   end
-  
+
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
+    end
   end
 end
